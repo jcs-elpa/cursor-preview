abfaf5656f3697e6fabfc440de2ef506177997c6
JenChieh <jcs090218@gmail.com>
2020-10-27 16:15:31 +0800

    Bump version.

--- cursor-preview.el
+++ cursor-preview.el
@@ -6,7 +6,7 @@
 ;; Author: Shen, Jen-Chieh <jcs090218@gmail.com>
 ;; Description: Preview cursor movement while execute command interactively.
 ;; Keyword: interactive preview cursor movement display
-;; Version: 0.1.0
+;; Version: 0.1.1
 ;; Package-Requires: ((emacs "24.3"))
 ;; URL: https://github.com/jcs-elpa/cursor-preview
 
